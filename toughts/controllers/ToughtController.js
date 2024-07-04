e6f0962cd961987af41437932e68cc9c0c55aae1
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2024-07-04 14:34:28 -0300

    Third
    Third

--- toughts/controllers/ToughtController.js
+++ toughts/controllers/ToughtController.js
@@ -1,9 +1,41 @@
 const Tought = require("../models/Tought.js");
 const User = require("../models/User.js");
 
+const { Op } = require("sequelize");
+
 module.exports = class ToughtController {
   static async showToughts(req, res) {
-    res.render("toughts/home");
+    let search = "";
+
+    if (req.query.search) {
+      search = req.query.search;
+    }
+
+    let order = "DESC";
+
+    if (req.query.order === "old") {
+      order = "ASC";
+    } else {
+      order = "DESC";
+    }
+
+    const toughtsData = await Tought.findAll({
+      include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
+      order: [["createdAt", order]],
+    });
+
+    const toughts = toughtsData.map((result) => result.get({ plain: true }));
+
+    let toughtsQty = toughts.length;
+
+    if (toughtsQty === 0) {
+      toughtsQty = false;
+    }
+
+    res.render("toughts/home", { toughts, search, toughtsQty });
   }
 
   static async dashboard(req, res) {
@@ -64,6 +96,33 @@ module.exports = class ToughtController {
       await Tought.destroy({ where: { id: id, UserId: UserId } });
 
       req.flash("message", "Pensamento removido com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/toughts/dashboard");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async updateTought(req, res) {
+    const id = req.params.id;
+
+    const tought = await Tought.findOne({ where: { id: id }, raw: true });
+
+    res.render("toughts/edit", { tought });
+  }
+
+  static async updateToughtSave(req, res) {
+    const id = req.body.id;
+
+    const tought = {
+      title: req.body.title,
+    };
+
+    try {
+      await Tought.update(tought, { where: { id: id } });
+      req.flash("message", "Pensamento atualizado com sucesso!");
 
       req.session.save(() => {
         res.redirect("/toughts/dashboard");
